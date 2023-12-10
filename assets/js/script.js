67f5ea44f63479e62f96ee63f3888fab8b56b62c
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-12-10 12:39:45 +0300

    finished header

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,29 @@ const addEventonElements = function(elements, eventType, callback) {
     }
 }
 
+/**
+ * Mobile navbar toggler
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]")
+const toggleNav = () => navbar.classList.toggle("active");
+
+addEventonElements(navTogglers, "click", toggleNav)
+
+
+/**
+ * HEADER ANIMATION
+ * when scrolled done to 100px header will be active
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+        header.classList.add("active");
+    }
+    else {
+        header.classList.remove("active");
+    }
+})
