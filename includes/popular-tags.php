1f5748e0b416d2b659a0b182f1798d3b1997d5fc
patrikkusnir1 <patrikkusnir1@gmail.com>
2026-09-19 06:51:48 +0300

    move popular tags section into separate file

---
+++ includes/popular-tags.php
@@ -0,0 +1,21 @@
+<!-- 
+#POPULAR TAGS
+-->
+
+<section class="tags" aria-labelledby="tag-label" id="tag-label">
+    <div class="container">
+        <h2 class="headline headline-2 section-title">
+            <span class="span">Popular Tags</span>
+        </h2>
+
+        <p class="section-text">
+            Most searched keywords
+        </p>
+
+        <ul class="grid-list">
+        <?php
+            show_categories( $topics );
+        ?>
+        </ul>
+    </div>
+</section>
--- index.php
+++ index.php
@@ -237,33 +237,11 @@ require 'includes/header.php';
                 include 'includes/hero.php'; 
                 include 'includes/topics-slider.php';
                 include 'includes/feature-posts.php';
+                include 'includes/popular-tags.php';
             ?>
 
 
 
-
-
-            <!-- 
-                #POPULAR TAGS
-            -->
-
-            <section class="tags" aria-labelledby="tag-label" id="tag-label">
-                <div class="container">
-                    <h2 class="headline headline-2 section-title">
-                        <span class="span">Popular Tags</span>
-                    </h2>
-
-                    <p class="section-text">
-                        Most searched keywords
-                    </p>
-
-                    <ul class="grid-list">
-                    <?php
-                        show_categories( $topics );
-                    ?>
-                    </ul>
-                </div>
-            </section>
 
             <!-- 
                 #RECENT POST
